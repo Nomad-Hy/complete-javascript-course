880d72a2bb65cc48ea9ea07c5fae0ed7c91de6f0
Hoyun choi <hoyunchoe123@gmail.com>
2024-10-05 07:22:31 +0900

    변수와 값의 의미와 실습

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,7 @@
+let country = "South Korea";
+let continent = "Asia";
+let population = "40million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let js = "amazing";
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas"); //value
+console.log(23);
+
+let firstName = "Yoon";
+
+console.log(firstName);
+
+let myGoalJob = "Developer";
+let myCurrentJob = "Student";
+
+console.log(myGoalJob);
+console.log(myCurrentJob);
